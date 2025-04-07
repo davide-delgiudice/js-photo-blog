18b717e9bd440daab974973a1b94fad3b0b71a84
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-07 16:10:19 +0200

    [UPD] create js function for HTML card

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 // dichiaro una variabile cotenente l'endpoint dell'api che ci serve
 const endPoint = `https://lanciweb.github.io/demo/api/pictures/`;
 
+// creiamo una funzione che ci permetta di aggiungere le i dati dell'API nei campi della card
 
-// copio la singola card scritta in HTML
+const createSummerCard = (cards) => {
 
-{/* <div class="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center">
-    <div class="card p-3 mb-3">
-        <div class="img-card">
-            <img src="./assets_day1/img/pin.svg" class="pin" alt="">
-            <img src="https://greatbarrierreeftours.com/files/2024/10/australias-whitehaven-beach-300x300.jpg" width="100%" alt="">
-        </div>
-        <div class="card-body p-0 mt-2">
-            <p class="m-0">data</p>
-            <p class="fw-bold m-0">ESTATE</p>
-        </div>
-    </div>
-</div> */}
+    // aggiungo la card scritta in HTML in una nuova variabile
+    const summerCard = `<div class="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center">
+                    <div class="card p-3 mb-3">
+                        <div class="img-card">
+                            <img src="./assets_day1/img/pin.svg" class="pin" alt="">
+                            <img src="https://greatbarrierreeftours.com/files/2024/10/australias-whitehaven-beach-300x300.jpg" width="100%" alt="">
+                        </div>
+                        <div class="card-body p-0 mt-2">
+                            <p class="m-0">data</p>
+                            <p class="fw-bold m-0">ESTATE</p>
+                        </div>
+                    </div>
+                </div>`;
+}
