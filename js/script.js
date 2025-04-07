87124b160b35a15c773f97ce075c3f6c3e273dc6
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-07 16:01:47 +0200

    [UPD] add js endPoint

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// dichiaro una variabile cotenente l'endpoint dell'api che ci serve
+const endPoint = `https://lanciweb.github.io/demo/api/pictures/`;
+
+
+// copio la singola card scritta in HTML
+
+{/* <div class="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center">
+    <div class="card p-3 mb-3">
+        <div class="img-card">
+            <img src="./assets_day1/img/pin.svg" class="pin" alt="">
+            <img src="https://greatbarrierreeftours.com/files/2024/10/australias-whitehaven-beach-300x300.jpg" width="100%" alt="">
+        </div>
+        <div class="card-body p-0 mt-2">
+            <p class="m-0">data</p>
+            <p class="fw-bold m-0">ESTATE</p>
+        </div>
+    </div>
+</div> */}
