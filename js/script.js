928542e10259ce2516c375ec4b2cc9f8f96a571e
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-09 11:50:38 +0200

    [UPD] add comments

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
+// dichiaro delle variabili che andranno a selezionare degli elementi in HTML tramite ID
 const overlay = document.getElementById('overlay')
 const button = document.getElementById('over-btn');
 
-
-// dichiaro una variabile cotenente l'endpoint dell'api che ci serve
+// dichiaro una variabile cotenente l'URL dell'API che avrà i dati per riempire la card
 const endPoint = `https://lanciweb.github.io/demo/api/pictures/`;
 
-// creiamo una funzione che ci permetta di aggiungere le i dati dell'API nei campi della card
 
+// creiamo una funzione che ci permetta di aggiungere le i dati dell'API nei campi della card
 const createSummerCard = (cards) => {
     // aggiungo la card scritta in HTML in una nuova variabile
     const summerCard = `<div class="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center">
@@ -26,30 +26,37 @@ const createSummerCard = (cards) => {
     document.getElementById('summerRow').innerHTML += summerCard;
 }
 
+
+// creo una funzione che andrà ad inserire nell'elemento con ID over-img, ovvero l'immagine all'interno dell'overlay, l'immagine della card tramite URL
 const createOverlayCard = (cardsUrl) => {
-    const overlayCard = `<img src = "${cardsUrl}">`;
+    const overlayCard = `<img src ="${cardsUrl}">`;
 
     document.getElementById('over-img').innerHTML = overlayCard;
 }
 
-// effettuo la chiamata get all'API per ottenere i dati specificati nella Card 
+
+// creo una funzione che effetua la chiamata GET all'API per ottenere i dati specificati nella Card 
 const getCards = () => {
     axios.get(endPoint).then(resp =>{
         
-        //per ogni card restituita chiamiamo createSummerCard
+        // i dati ottenuti dall'API vengono memorizzati in generateArray
         const generateArray = resp.data;
         console.log(generateArray);
 
+        // per ogni elemento dell'array, viene chiamata la funzione createSummerCard
         generateArray.forEach(card => {
             createSummerCard(card);
         });
 
+        // selezioniamo tutte le card con classe .card nella pagina dichiarando una nuova variabile
         const summerImgs = document.querySelectorAll('.card');
 
+        // aggiungiamo un evento di click per ogni card che rimuove la classe hide dell'overlay
         summerImgs.forEach( (card) => {
             card.addEventListener('click', () => {
                 overlay.classList.remove('hide');
                 
+                //  condizione per controllare la card che stiamo andando a cliccare e assocciare la corretta immagine al click grazie alla posizione, riprendendo la funzione createOverlayCard
                 if(card.classList.contains(1)){
                     createOverlayCard(generateArray[0].url);
                 }
@@ -73,6 +80,7 @@ const getCards = () => {
     });
 };
 
+// aggiungo un addEvent per reinserire l'hide al click del bottone
 button.addEventListener('click', () => {
     overlay.classList.add('hide');
 })
